8455f256abadfd49dd83e57eede104d9d8cbe78f
Charles Butler <chuck@dasroot.net>
2014-10-29 01:22:59 -0400

    Adds caching support to reduce over all youtube requests

--- lazychannel/processor/youtube.py
+++ lazychannel/processor/youtube.py
@@ -5,10 +5,10 @@ import shlex
 import os
 
 class youtube:
-    def __init__(self):
-        self.log = logging.getLogger('worker.youtube')
+    def __init__(self, cache_file):
+        self.log = logging.getLogger('lazychannel.worker.youtube')
         self.BASE_URL = "http://gdata.youtube.com/feeds/api/videos?max-results=1&alt=json&orderby=published&author={}"
-
+        self.cache_file = cache_file
 
     def fetch_channel(self, uuid):
         channel_url = self.BASE_URL.format(uuid)
@@ -21,14 +21,26 @@ class youtube:
             self.log.error('No feed found, assuming account deleted.')
             return
         for v in videos['feed']['entry']:
-            self.log.debug('fetching {}'.format(v['title']['$t']))
-            self.call_downloader(out, v['link'][0]['href'])
+            link = v['link'][0]['href']
+            if not self.in_cache(link):
+                self.log.info('Fetching {}'.format(v['title']['$t']))
+                self.call_downloader(out, link)
+            self.log.debug('Cache Hit on: {} - skipping'.format(v['title']['$t']))
+
+    def in_cache(self, link):
+        with open(self.cache_file, 'r') as f:
+            cache = f.readlines()
+        if link in cache:
+            return True
+        return False
 
     def call_downloader(self, outpath, link):
         outpath = "{}{}%(title)s.%(ext)s".format(outpath, os.path.sep)
         c = "youtube-dl -x --audio-format=mp3 -o {} {}".format(outpath, link)
         cmd = shlex.split(c)
         subprocess.check_output(cmd)
+        with open(self.cache_file, 'a+') as f:
+            f.write("{}\n".format(link))
 
 
 #

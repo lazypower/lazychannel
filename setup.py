53b6280e7317168575061cbf3d8e28ec997f66e0
Charles Butler <chuck@dasroot.net>
2014-10-29 11:13:15 -0400

    fix dependencies in setup script

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="lazychannel",
-    version="0.1.0",
+    version="0.1.1",
     description="Fetch things daily!",
     license="LGPLv3",
     packages=find_packages(),
@@ -14,7 +14,7 @@ setup(
     },
     install_requires=[
         'requests',
-        'gdata',
+        'pyyaml',
     ],
 )
 

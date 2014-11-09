082536a5739c0cd5c360ca357634037cad7eeb59
Charles Butler <chuck@dasroot.net>
2014-11-09 11:03:10 -0500

    Moves output_dir to helpers for use elsewhere

--- lazychannel/helpers.py
+++ lazychannel/helpers.py
@@ -1,5 +1,12 @@
 import os
+import logging
 
+log = logging.getLogger('lazychannel.helpers')
 
 def pexpand(path):
     return os.path.abspath(os.path.expanduser(path))
+
+def output_dir(out):
+    if not os.path.exists(out):
+        log.info("Creating directory {}".format(out))
+        os.makedirs(out)
